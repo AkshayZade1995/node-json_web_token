481b17328ab85166900d73911fdc8722625f45da
Akshay <akshayzade.runtimesolutions@gmail.com>
2020-09-28 01:41:57 +0530

    basic setup

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const User = require('./models/users');
+
+mongoose.connect('mongodb+srv://avenger:Lr2nN3zVGaXNKGcw@cluster0.cvfas.mongodb.net/tutorial?retryWrites=true&w=majority',
+    {
+        useNewUrlParser:true,
+        useUnifiedTopology:true
+    }
+).then(()=>{
+    console.warn('connected');
+})
+
+app.get('/',function(req,res){
+    res.end('Hello')
+});
+app.listen(4000);
