98ff0969f86bbe8f8492a14ce4ebacbf914bbf19
Akshay <akshayzade.runtimesolutions@gmail.com>
2020-09-29 11:05:41 +0530

    user registration with encrypted password

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+const jsonParser = bodyParser.json();
+const crypto = require('crypto')
+var key ='password';
+var algo = 'aes256';
 const User = require('./models/users');
 
 mongoose.connect('mongodb+srv://avenger:Lr2nN3zVGaXNKGcw@cluster0.cvfas.mongodb.net/tutorial?retryWrites=true&w=majority',
@@ -9,10 +14,23 @@ mongoose.connect('mongodb+srv://avenger:Lr2nN3zVGaXNKGcw@cluster0.cvfas.mongodb.
         useUnifiedTopology:true
     }
 ).then(()=>{
-    console.warn('connected to mongodb terminal');
+    console.warn('connected');
 })
 
-app.get('/',function(req,res){
-    res.end('Hello')
+app.post('/register',jsonParser,function(req,res){
+    var cipher = crypto.createCipher(algo,key); 
+    var encrypted = cipher.update(req.body.password,'utf8','hex')+cipher.final('hex');
+
+    var data = new User({
+        _id:mongoose.Types.ObjectId(),
+        name:req.body.name,
+        email:req.body.email,
+        password:encrypted,
+        });
+    
+    data.save().then((result)=>{
+        res.json(result)
+    })
+    
 });
 app.listen(4000);
